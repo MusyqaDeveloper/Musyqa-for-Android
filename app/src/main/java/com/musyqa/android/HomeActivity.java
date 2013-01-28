1560aa41960bd07fbf7351e2a7626b49f7fa8962
MusyqaDeveloper <roi@musyqa.com>
2013-01-28 22:38:40 +0200

    first smoke run works

---
+++ app/src/main/java/com/musyqa/android/HomeActivity.java
@@ -0,0 +1,18 @@
+
+package com.musyqa.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+    
+    /*
+     * (non-Javadoc)
+     * @see android.app.Activity#onCreate(android.os.Bundle)
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
