b2b9ab258775cfe35dd4c048effd54af8e5b408a
MusyqaDeveloper <roi@musyqa.com>
2013-01-29 00:26:24 +0200

    testGetMixsesWithPagingReturnRequestsMixes test pass

--- app/src/main/java/com/musyqa/android/eighttracks/EightTracksHttpClient.java
+++ app/src/main/java/com/musyqa/android/eighttracks/EightTracksHttpClient.java
@@ -2,45 +2,110 @@
 package com.musyqa.android.eighttracks;
 
 import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.kevinsawicki.http.HttpRequest;
 import com.github.kevinsawicki.http.HttpRequest.HttpRequestException;
 import com.musyqa.android.Config;
 
 import java.io.IOException;
+import java.io.Reader;
 
 /**
  * The Class EightTracksHttpClient.
  */
 public class EightTracksHttpClient {
 
-    public MixSet getMixSet() {
-        HttpRequest httpRequest = HttpRequest.get("http://8tracks.com/mixes.json");
-        httpRequest.header("X-Api-Key", Config.ApiKeys.EIGHT_TRACKS);
+    private static final String BASE_URL = "http://8tracks.com/";
 
-        if (httpRequest.ok()) {
-            ObjectMapper objectMapper = new ObjectMapper();
-            MixSet mixSet;
-            try {
-                mixSet = objectMapper.readValue(httpRequest.bufferedReader(), MixSet.class);
-                return mixSet;
-            } catch (JsonParseException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (JsonMappingException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (HttpRequestException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            return null;
-        } else {
-            return null;
+    private ObjectMapper mObjectMapper = new ObjectMapper();
+
+    /**
+     * Gets the mix set.
+     * 
+     * @return the mix set
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    public MixSet getMixSet() throws IOException {
+        try {
+            return fromJson(ok(get("mixes.json")), MixSet.class);
+        } catch (HttpRequestException e) {
+            throw e.getCause();
         }
+    }
+
+    /**
+     * Gets the mix set.
+     * 
+     * @param perPage the per page
+     * @param page the page
+     * @return the mix set
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    public MixSet getMixSet(int perPage, int page) throws IOException {
+        try {
+            String url = String.format("mixes.json?per_page=%d&page=%d", perPage, page);
+            return fromJson(ok(get(url)), MixSet.class);
+        } catch (HttpRequestException e) {
+            throw e.getCause();
+        }
+    }
+
+    /**
+     * Create object of class type from content of request
+     * 
+     * @param request
+     * @param target
+     * @return object of target class type
+     * @throws IOException
+     */
+    protected <V> V fromJson(final HttpRequest request, final Class<V> clazz)
+            throws IOException {
+
+        final Reader reader = request.bufferedReader();
+
+        try {
+
+            return mObjectMapper.readValue(reader, clazz);
+
+        } catch (JsonParseException e) {
+
+            IOException ioException = new IOException("Parsing JSON failed");
+            ioException.initCause(e);
+            throw ioException;
+
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException ignored) {
+                // Ignored
+            }
+        }
+    }
+
+    /**
+     * Perform GET request.
+     * 
+     * @param url the url
+     * @return the http request
+     */
+    protected HttpRequest get(final String url) {
+        return HttpRequest
+                .get(BASE_URL + url)
+                .header("X-Api-Key", Config.ApiKeys.EIGHT_TRACKS);
+    }
+
+    /**
+     * Verify request response code is a 200 OK and throw an exception when it
+     * is not
+     * 
+     * @param request
+     * @return request
+     * @throws IOException
+     */
+    protected HttpRequest ok(HttpRequest request) throws IOException {
+        if (!request.ok())
+            throw new IOException("Unexpected response code of "
+                    + request.code());
+        return request;
     }
 }
--- integration-tests/src/main/java/com/musyqa/android/eighttracks/EightTracksHttpClientTest.java
+++ integration-tests/src/main/java/com/musyqa/android/eighttracks/EightTracksHttpClientTest.java
@@ -3,6 +3,8 @@ package com.musyqa.android.eighttracks;
 
 import android.test.AndroidTestCase;
 
+import java.io.IOException;
+
 /**
  * The Class EightTracksHttpClientTest.
  */
@@ -10,13 +12,33 @@ public class EightTracksHttpClientTest extends AndroidTestCase {
 
     /**
      * Test get mixes should return 12 mixes.
+     * 
+     * @throws IOException
      */
-    public void testGetMixesShouldReturn12Mixes() {
+    public void testGetMixesShouldReturn12Mixes() throws IOException {
         EightTracksHttpClient httpClient = new EightTracksHttpClient();
         MixSet mixSet = httpClient.getMixSet();
 
         assertNotNull("mixSet should not be null", mixSet);
         assertNotNull("mixes collection should not be null", mixSet.getMixes());
         assertEquals("mixes size should be 12", 12, mixSet.getMixes().size());
+    }
+
+    /**
+     * Test get mixses with paging return requests mixes.
+     * 
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    public void testGetMixsesWithPagingReturnRequestsMixes() throws IOException {
+        int perPage = 25;
+        int pageNumber = 3;
+
+        EightTracksHttpClient httpClient = new EightTracksHttpClient();
+        MixSet mixSet = httpClient.getMixSet(perPage, pageNumber);
+
+        assertNotNull("mixSet should not be null", mixSet);
+        assertNotNull("mixes collection should not be null", mixSet.getMixes());
+        assertEquals("mixes size should be " + perPage, perPage, mixSet.getMixes().size());
+        assertEquals("mixset page should be " + pageNumber, pageNumber, mixSet.getPage());
     }
 }
