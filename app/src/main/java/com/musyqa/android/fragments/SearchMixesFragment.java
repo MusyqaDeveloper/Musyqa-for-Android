8aefe2029a1b86bc693eee3086b50f78ec283787
MusyqaDeveloper <roi@musyqa.com>
2013-01-29 22:15:41 +0200

    started working on SearchMixesFragment

---
+++ app/src/main/java/com/musyqa/android/fragments/SearchMixesFragment.java
@@ -0,0 +1,20 @@
+package com.musyqa.android.fragments;
+
+import com.musyqa.android.R;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * User: roisagiv Date: 29/1/13 Time: 10:12 PM
+ */
+public class SearchMixesFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search_mixes, container);
+    }
+}
